3414807ebad50f263ec2e830fbd8397bc59ec82b
mek217 <mek217@lehigh.edu>
2014-08-28 21:01:48 +0000

    updated hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Matthew Koh
+//Hw 01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args) {
+        
+        // print the statement with a box around it
+        System.out.println("-----------");
+        System.out.println("| Welcome |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-M--E--K--2--1--7-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("I am an archer. The word 'arching' is actually incorrect,");
+        System.out.println("because there is no verb for the act of doing archery.");
+        System.out.println("test");
+        
+        //test/testest
+    }
+}
