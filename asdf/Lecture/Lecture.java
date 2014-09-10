bbf059e89bc2a118ccae86636a0e68e4ad4b6b03
mek217 <mek217@lehigh.edu>
2014-09-10 20:23:57 +0000

    made Lecture Dummy Prog

---
+++ asdf/Lecture/Lecture.java
@@ -0,0 +1,10 @@
+public class Lecture{
+    public static void main(String[] args) {
+        double myDouble1 = 3.2;
+        double myDouble2 = 4.5;
+        int myVal = (int) (myDouble1 + myDouble2);
+        
+        System.out.println("myVal is equal to "+myVal);
+        
+    }
+}
