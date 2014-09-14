e651b097caca3cf92c6c45761face40cc65db810
mek217 <mek217@lehigh.edu>
2014-09-14 16:24:03 +0000

    Fixed date

--- asdf/Lecture/Lecture.java
+++ asdf/Lecture/Lecture.java
@@ -1,10 +1,8 @@
 public class Lecture{
     public static void main(String[] args) {
-        double myDouble1 = 3.2;
-        double myDouble2 = 4.5;
-        int myVal = (int) (myDouble1 + myDouble2);
-        
-        System.out.println("myVal is equal to "+myVal);
+        double myNumber1 = 3;
+        myNumber1 *= 2;
+        System.out.println("myNumber1 = "+myNumber1);
         
     }
 }
