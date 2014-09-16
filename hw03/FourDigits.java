a6ebaf33faebace042bffa30a4f856d26a5b1cb4
mek217 <mek217@lehigh.edu>
2014-09-16 00:56:29 +0000

    updated Four Digits program

--- hw03/FourDigits.java
+++ hw03/FourDigits.java
@@ -19,22 +19,23 @@ public class FourDigits{
         //Call scanner constructor
         myScanner = new Scanner( System.in );
         
-        //
+        //Prompt user to enter a double
         System.out.print("Enter a double to get the 4 digits after the decimal point: ");
         
         //Accept user input
         double number = myScanner.nextDouble();
         
-        //Figure out what this shit does
+        //Move each decimal that will be saved to it's own variable
         int tenths = (int)(number*10)%10;
         int hundredths = (int)(number*100)%10;
         int thousandths = (int)(number*1000)%10;
         int tenThousandths = (int)(number*10000)%10;
         
-        int fourDigit = tenths*10+hundredths*100+thousandths*1000+tenThousandths*10000;
+        //Combine the 4 decimals into a presentable output vairable
+        int fourDigit = tenths*1000+hundredths*100+thousandths*10+tenThousandths;
         
         //Print output
-        System.out.println("The first four digits to the right of the decimal of the entered number are: "+fourDigit);
+        System.out.println("The first 4 digits to the right of the decimal: "+fourDigit);
         
     }
 }
