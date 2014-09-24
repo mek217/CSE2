bd2df05a596f36755f8b8469d62f1f6941d094f3
mek217 <mek217@lehigh.edu>
2014-09-24 03:33:10 +0000

    updated lab04

--- lab04/BigMacAgain.java
+++ lab04/BigMacAgain.java
@@ -30,60 +30,69 @@ public class BigMacAgain {
         final double BIGMAC_PRICE$ = 2.22;
         final double FRIES_PRICE$ = 2.15;
         
-        //Valdate input: The number of Big Macs is an integer
-        if (myScanner.hasNextInt()) {
-            
-            if (nBigMacs>0) {
-                
-                //Prompt user for number of Big Macs
-                System.out.print("Enter the number of Big Macs: ");
+        //Prompt user for number of Big Macs
+        System.out.print("Enter the number of Big Macs: ");
         
-                //Accept user input
-                int nBigMacs = myScanner.nextInt();
+        //Valdate input: The number of Big Macs is an integer
+        if (myScanner.hasNextInt()){
+            
+            //Accept user input
+            int nBigMacs = myScanner.nextInt();
+            
+            if (nBigMacs > 0){
                 
                 //Calculate price of all BigMacs
                 double priceAllBigMacs$ = BIGMAC_PRICE$*nBigMacs;
-                
-                //Print #BigMacs, cost per BigMac, and price of all BigMacs
                 System.out.println ("You ordered " +nBigMacs+ " Big Macs for a cost of " +nBigMacs+ 'x' +BIGMAC_PRICE$+ "= $" +priceAllBigMacs$+ '.');
+                
+                //Prompt user for fries or no fries
+                System.out.print("Do you want an order of fries (Y/y/N/n)?");
+                
+                //Accept user input
+                String friesQuestion = myScanner.next();
+                
+                
+                
+                
+                
+                
+                
+                if (friesQuestion.equals("Y") || friesQuestion.equals("y")){
+                    
+                    //Print that user wanted fries
+                    System.out.println("You ordered fries at a cost of $" +FRIES_PRICE$+ ".");
+                    
+                    //calculate price of meal
+                    double mealPrice$ = priceAllBigMacs$+FRIES_PRICE$;
+                    
+                    //Print cost of meal
+                    System.out.println("The total cost of the meal is $" +mealPrice$+ '.');
+                    
+                }
+                else if (friesQuestion.equals("N") || friesQuestion.equals("n")){
+                    
+                    //Print cost of meal
+                    System.out.println("The total cost of the meal is $" +priceAllBigMacs$+ '.');
+                }
+                else{
+                    System.out.println("You did not enter 'Y', 'y', 'N', or 'n'");
+                    return;                 //leaves program, i.e.
+                                                //program terminates
+                }
             }
-            else {
-                System.out.println ("You did not enter an int > 0.");
+            else{
+                System.out.println("You did not enter an int >0");
+                return;                     //leaves program, i.e.
+                                                //program terminates
             }
         }
-        else {
+        else{
             System.out.println("You did not enter an int");
-            return;                         //leaves  program, i.e.
-                                                // program terminates
+            return;                         //leaves program, i.e.
+                                                //program terminates
         }
-        
-        //Prompt user for fries or no fries
-        System.out.print("Do you want an order of fries (Y/y/N/n)?");
-        
-        //Accept user input
-        String userInput = myScanner.next();
-        
-        //Valie input: entered string must equal Y, y, N, or n
-        if (userInput.equals("Y") || userInput.equals("y")) {
-            
-            //Print that user wanted fries
-            System.out.println("You ordered fries at a cost of $" +FRIES_PRICE$+ '.');
-            
-            //Calculate price of  meal
-            double mealPrice$ = priceAllBigMacs$+FRIES_PRICE$;
-            
-            //Print cost of meal
-            System.out.println("The total cost of the meal is $" +mealPrice$+ '.');
-        }
-        else if (userInput.equals("N") || userInput.equals("n")) {
-            
-            //Print cost of meal
-            System.out.println("The total cost of the meal is $" +priceAllBigMacs$+ '.');
-        }
-        else {
-            System.out.println("You did not enter 'Y', 'y', 'N', or 'n'");
-            return;
-            
-        }
+
+
+
     }
 }
