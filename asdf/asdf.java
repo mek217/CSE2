0fc7998ba3dbd9409cfee41229a647f28aeb3e3e
mek217 <mek217@lehigh.edu>
2014-10-21 01:25:16 +0000

    updated lab08

--- asdf/asdf.java
+++ asdf/asdf.java
@@ -5,26 +5,70 @@
 import java.util.Scanner;
 public class asdf{
     public static void main(String[] args){
-        Scanner myScan;
-        myScan = new Scanner(System.in);
         
+        Scanner scan = new Scanner(System.in);
         
-        int i = 120;
-        int j = 0;
+        int n = 0;
         
-        do{
-            i = i-6;
-            System.out.println("i: " + i);
+        System.out.print("Enter an int- ");
+        if(!scan.hasNextInt()){
+            do{
+                scan.next(); //get rid of the junk entered by user
+                System.out.print("You did not enter an int; try again- ");
+            }while(!scan.hasNextInt());
+        }
+        
+        n = scan.nextInt();
+        
+        int j=0, k=0;
+        
+        while(j < n && j < 40){
             
-            for(j = 0; j<5; j++){
-                System.out.println("j: " + j);
+            k=0;
+            while(k < j + 1){
+                System.out.print('*');
+                k++;
             }
-        }while(i > 0);
+            System.out.println();
+            
+            j++;
+        }
         
-        //for (i = 0; i < 5; system.out.print("enter an int") ){
-        //    i = myScan.nextInt();
-        //    System.out.println("i: " + i);
-        //}
-
+        k = 4;
+        
+        System.out.println("k=" + k);
+        k++;
+        while(k < 4){
+            System.out.println("k=" + k);
+            k++;
+        }
+        
+        for(int count=0; true; count++){
+            if(count == 0){
+                count++;
+            }
+            if(n == 1){
+            }
+            if(n == 1 || n == 2){
+                System.out.print("Case 2 ");
+                continue;
+            }
+            if(n == 1 || n == 2 || n == 3){
+                break;
+            }
+            if(n == 4){
+                System.out.println("Case 4");
+            }
+            if(n == 4 || n == 5){
+                System.out.println("Case 5");
+            }
+            else{
+                System.out.println(n + " is > 5 or <1");
+            }
+            System.out.println("count: " + count);
+            if(count > 3){
+                break;
+            }
+        }
     }
 }
