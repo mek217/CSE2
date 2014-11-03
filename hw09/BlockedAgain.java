7d3d5ca4ab127e1467207fbe25c16d9288d8b0c2
mek217 <mek217@lehigh.edu>
2014-11-03 17:25:21 +0000

    started hw09

--- hw09/BlockedAgain.java
+++ hw09/BlockedAgain.java
@@ -1,17 +1,25 @@
 ////////////////////////////////////////////////////////////////////////////
 //Matthew Koh
-////14
-//Lab/Hw
-//Program Name
+//11/3/14
+//Hw09
+//Program BlockedAgain
 //
-//Start
+/*
+Problems & Solutions
+[A]: getInt(): Must ask user for an acceptible size of desired block output
+    [A1]: checkInt(): input is an int
+    [A2]: checkRange(): input is positive
+[B]: allBlocks(int x): Must print the 'stack' of blocks
+    [B1]: block(): print each individual block with same width
+        [B1a]: line(): generate each line with the right numbers and indent distance
+*/
 
 import java.util.Scanner;
-public class BlockedAgain{
-public static void main(String []s){
-int m;
-//force user to enter int in range 1-9, inclusive.
-m = getInt(); 
-allBlocks(m);
-}
+public class BlockedAgain {
+    public static void main(String[] s) {
+        int m;
+        //force user to enter int in range 1-9, inclusive.
+        m = getInt();
+        allBlocks(m);
+    }
 }
