9ad7a27e7f47e211c7ae328f52ba071cfaaa3857
mek217 <mek217@lehigh.edu>
2014-11-12 16:42:02 +0000

    made lab11

--- asdf/asdf.java
+++ asdf/asdf.java
@@ -11,168 +11,40 @@
 //Start
 
 import java.util.Scanner;
-
-import java.util.Scanner;
 public class asdf {
-    public static void main(String[] arg) {
-        int m;
-        //force user to enter int in range 1-9, inclusive.
-        m = getInt();
-        allBlocks(m);
-    }
-/*
-Problems & Solutions
-[B]: allBlocks(int x): Must print the 'stack' of blocks
-    [B1]: block(): print each individual block with same width
-        [B1a]: line(): generate each line with the right numbers and indent distance
-*/
-
-    public static int getInt() {                                                //[A]: getInt(): Prompt user for an int
-        Scanner scan;
-        scan = new Scanner(System.in);
-        boolean isDigit = false;
-        int userInput = 0;
-        System.out.print("Enter an int between 1 and 9, inclusive: ");
-        while (isDigit != true){
-            userInput = checkInt(scan);                                         //[A1]: checkInt()
-            isDigit = checkRange(userInput);                                    //[A2]: checkRange()
-            if(isDigit == false){
-                System.out.print("You did not enter an int in [1,9]; try again: ");
-                continue;
-            }else{break;}
-        }
-        return userInput;
-    }
-
-    public static int checkInt(Scanner scan){                                   //[A1]: checkInt(): input is an int
-        while(true){
-            if(scan.hasNextInt()){
-                int userNumber = scan.nextInt();
-                boolean ifPositive = checkRange(userNumber);
-                return userNumber;
-            }
-            else{
-                System.out.print("You did not enter an int. try again: ");
-                scan.next();
-                continue;
-            }
-        }
-    }
-
-    public static boolean checkRange(int testInt){                              //[A2]: checkRange(): input is positive
-        boolean posTest = (testInt >= 1 && testInt <= 9)?true:false;
-        return posTest;
-    }
-    
-/*
-ALL BLOCKMAKING VARIABLES
-All variables: int x, int whichBlock, int whichLineInBlock, int whichNumberInLine, int whichSpace, int numNums, int spaces, int numBlock, int dashes, dashes
-
-Sentry Variables: REMEMBER TO RESET
-which Block
-    increment up
-        whichBlock = 1
-            In: allblocks, block, line
-which Line
-    increment up
-        whichLineInBlock = 1
-            In: allblocks, block
-which Number
-    increment up
-        whichNumberInLine = 1
-            In: allblocks, block, line
-which Space
-    increment up
-        whichSpace = 1
-            In: line
-            
-Actual Variables:
-
-how many total blocks are being made(in the program)?
-    int x = # blocks
-        The user input x: there will be x blocks.
-            In: allblocks, block, line
-    
-how many total lines are being entered (in the block)?
-    int whichBlock = # lines
-        Whichever block you are on will have that many lines in the block.
-            In: allblocks, block, line
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
         
-how many total numbers being printed(in the line)?
-    int numNums = 2*whichBlock-1 = # numbers
-        Whichever block you are on will have (2*whichBlock-1) numbers printed on the line.
-            In: line
+        final int TEN_INT_ARRAY = 10;
+        int[] tenArray = new int[TEN_INT_ARRAY];
         
-how many spaces (on left and right)?
-    int spaces = x - whichBlock
-        Every line will have  ((2x-1) - (2*whichBlock-1))/2
-                            = (2x-1-2*whichBlock+1)/2
-                            = (2x-2*whichBlock/2)
-                            = 2(x-whichBlock)/2
-                            = (x-whichBlock) spaces to the left and to the right of the printed numbers.
-            In: line
-
-what about the lines after each block?
-    Make sure to print a line when 
-        HAVE numBlock EQUAL dashes INSTEAD OF whichBlock WHEN PRINTING THE ADDITIONAL LINE
-    PRINT AN ADDITIONAL LINE WITH -'S INSTEAD OF whichBlock WHEN whichBlock == whichLineInBlock
-*/
-    public static void allBlocks(int x){
-        int whichBlock = 1;
-        for(whichBlock = 1; whichBlock <= x; whichBlock++){
-            block(x, whichBlock);
+        System.out.print("Enter 10 ints- ");                                    //Prompt user to enter 10 ints for array
+        for(int i=0; i<TEN_INT_ARRAY; i++){
+            tenArray[i] = scan.nextInt();
         }
-        return;
-        //All variables: int x, int whichBlock, int whichLineInBlock, int whichNumberInLine, int whichSpace, int numNums, int spaces, int numBlock, int dashes
-        //NEED: x
-        //MADE: whichBlock
+        
+        int min = tenArray[0], max = tenArray[0], sum = 0;                      //Search min and max entries
+        for(int j=0; j<TEN_INT_ARRAY; j++){
+            if(tenArray[j]<min){min = tenArray[j];}
+            if(tenArray[j]>max){max = tenArray[j];}
+            sum += tenArray[j];
+        }
+        
+        System.out.println("The lowest entry is " + min);                       //Print highest, lowest, and sum
+        System.out.println("The highest entry is " + max);
+        System.out.println("The sum is " + sum);
+        
+        int[] reverseArray = new int[TEN_INT_ARRAY];                             //Copy contents of the array into second arry in reverse order
+        for(int k=0; k<TEN_INT_ARRAY; k++){
+            reverseArray[k] = tenArray[(TEN_INT_ARRAY-1)-k];
+        }
+        
+        
+        for(int l=0; l<TEN_INT_ARRAY; l++){
+            String spacing = (tenArray[l]<0) ? "" : " ";                        //Account for number length
+            spacing = (tenArray[l]*tenArray[l]<100) ? spacing+"    " : spacing+"   ";
+            System.out.println("     " + tenArray[l] + spacing + reverseArray[l]);
+        }
+        
     }
-    
-    public static void block(int x, int whichBlock){
-        int whichLineInBlock = 1;
-        int numBlock = whichBlock, dashes = 0;
-        for(whichLineInBlock = 1; whichLineInBlock <= whichBlock; whichLineInBlock++){
-            line(x, whichBlock, whichLineInBlock, numBlock);
-            System.out.println();
-            if(whichLineInBlock == whichBlock){
-                line(x, whichBlock, whichLineInBlock, dashes);
-                System.out.println();
-            }
-        }
-        return;
-        //All variables: int x, int whichBlock, int whichLineInBlock, int whichNumberInLine, int whichSpace, int numNums, int spaces, int numBlock, int dashes
-        //NEED: x, whichBlock, 
-        //MADE: whichLineInBlock, numBlock, dashes
-    }
-    
-    public static void line(int x, int whichBlock, int whichLineInBlock, int numBlock){
-        int spaces = x - whichBlock;
-        int numNums = 2*whichBlock-1;
-        int whichSpace = 1;
-        int whichNumberInLine = 1;
-        for(whichSpace = 1; whichSpace <= spaces; whichSpace++){
-            System.out.print(' ');
-        }
-        for(whichNumberInLine = 1; whichNumberInLine <= numNums; whichNumberInLine++){
-            if(numBlock > 0){
-                System.out.print(numBlock);
-            }else{System.out.print("-");}
-            
-        }
-        for(whichSpace = 1; whichSpace <= spaces; whichSpace++){
-            System.out.print(' ');
-        }
-        //System.out.print("whichBlock: "+whichBlock+" whichLineInBlock: "+whichLineInBlock+" whichNumberInLine: "+whichNumberInLine);
-        return;
-        //All variables: int x, int whichBlock, int whichLineInBlock, int whichNumberInLine, int whichSpace, int numNums, int spaces, int numBlock, int dashes
-        //NEED: x, whichBlock, whichLineInBlock, numBlock/dashes
-        //MADE: spaces, numNums, whichSpace, whichNumberInLine
-    }
-
 }
-
-/*
-allBlocks(int x) (how many blocks) makes number of blocks
-block() (the stuff in each block) makes number of lines
-line() (the stuff in each line) makes proper numbers and indent spacings
-*/
---
+++ lab11/Arrays.java
@@ -0,0 +1,67 @@
+////////////////////////////////////////////////////////////////////////////
+//Matthew Koh
+//11/11/14
+//Lab 11
+//Program Arrays
+//
+//Create an array that can hold 10 ints
+//Ask the user enter 10 ints and store them in the array
+//Search the array for the highest entry and print it out
+//Search the array for the lowest entry and print it out
+//Sum the ints in the array and print out the sum
+//Copy the contents of the array into a second array in reverse order 
+//Print the contents of the two arrays in two columns, side by side. 
+//Below is sample output that your program should resemble:
+
+/*Enter 10 ints- -9 20 8 9 23 45 1 2 89 70
+The lowest entry is -9
+The highest entry is 89
+The sum is 258
+    -9     70
+    20     89
+     8      2
+     9      1
+    23     45
+    45     23
+     1      9
+     2      8
+    89     20
+    70     -9
+    */
+
+import java.util.Scanner;
+public class Arrays{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        
+        final int TEN_INT_ARRAY = 10;
+        int[] tenArray = new int[TEN_INT_ARRAY];
+        
+        System.out.print("Enter 10 ints- ");                                    //Prompt user to enter 10 ints for array
+        for(int i=0; i<TEN_INT_ARRAY; i++){
+            tenArray[i] = scan.nextInt();
+        }
+        
+        int min = tenArray[0], max = tenArray[0], sum = 0;                      //Search min and max entries
+        for(int j=0; j<TEN_INT_ARRAY; j++){
+            if(tenArray[j]<min){min = tenArray[j];}
+            if(tenArray[j]>max){max = tenArray[j];}
+            sum += tenArray[j];
+        }
+        
+        System.out.println("The lowest entry is " + min);                       //Print highest, lowest, and sum
+        System.out.println("The highest entry is " + max);
+        System.out.println("The sum is " + sum);
+        
+        int[] reverseArray = new int[TEN_INT_ARRAY];                             //Copy contents of the array into second array in reverse order
+        for(int k=0; k<TEN_INT_ARRAY; k++;){reverseArray[k] = tenArray[TEN_INT_ARRAY-k];}
+        
+        
+        for(int l=0; l<TEN_INT_ARRAY; l++){
+            String spacing = (tenArray[l]>=10) ? "    " : "     ";              //Account for number length
+            System.out.print("     " + tenArray[l] + spacing + reverseArray[l]);
+        }
+        
+        
+    }
+}
